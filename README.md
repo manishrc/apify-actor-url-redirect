f7d4c214dc479626b49388159ccb9d011193315d
Manish Chiniwalar <hi@manishrc.com>
2019-10-01 17:15:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Apify Actor - URL Redirects
+
+For a list of input URLs, get a list of final URLs that are loaded after redirects.
+
+## Input parameters
+
+<table>
+<thead>
+  <tr>
+    <td>Field</td>
+    <td>Type</td>
+    <td>Description</td>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>namespace</td>
+    <td>String</td>
+    <td>Uses this namespace to persist requests</td>
+  </tr>
+  <tr>
+    <td>sources</td>
+    <td>Array</td>
+    <td>(Option 1) Array of URLs to resolve</td>
+  </tr>
+  <tr>
+    <td>sources.requestsFromUrl</td>
+    <td>String</td>
+    <td>(Option 2) URL to a file with the list of URLs</td>
+  </tr>
+  <tr>
+  <td>crawlerOptionsOverrides</td>
+  <td>Object</td>
+  <td>Crawler options Overrides. [See for overrides ↗](https://sdk.apify.com/docs/api/basiccrawler#new-basiccrawleroptions)</td>
+  </tr>
+</tbody>
+</table>
+
+## Output
+
+```json
+{
+  "origionalUrl": "http://google.com/",
+  "normalizedUrl": "http://www.google.com",
+  "loadedUrl": "https://www.google.com",
+  "errorMessage": "<Short error message>",
+  "#isFailed": "<true/false>"
+}
+```
---
+++ main.js
@@ -0,0 +1,56 @@
+const Apify = require("apify");
+const normalizeUrl = require("normalize-url");
+const rp = require("request-promise");
+
+Apify.main(async () => {
+  const input = await Apify.getInput();
+  console.log("Input:");
+  console.dir(input);
+
+  if (!input || !input.sources)
+    throw new Error('Input must be a JSON object with the "sources" field!');
+
+  const { crawlerOptionsOverrides } = input;
+
+  const requestList = await Apify.openRequestList(
+    `redirect-request-list-${input.namespace || "default"}`,
+    input.sources
+  );
+
+  const basicCrawler = new Apify.BasicCrawler({
+    ...crawlerOptionsOverrides,
+    requestList,
+    handleRequestFunction: async ({ request }) => {
+      const origionalUrl = request.url;
+      const normalizedUrl = normalizeUrl(origionalUrl);
+
+      const loadedUrl = (await rp({
+        url: request.url,
+        followRedirect: true,
+        followAllRedirects: true,
+        resolveWithFullResponse: true
+      }))["request"]["uri"]["href"];
+
+      await Apify.pushData({
+        origionalUrl,
+        normalizedUrl,
+        loadedUrl
+      });
+    },
+
+    handleFailedRequestFunction: async ({ request }) => {
+      const origionalUrl = request.url;
+      const normalizedUrl = normalizeUrl(origionalUrl);
+      const errorMessage = (request.errorMessages[0] || "").split("\\n")[0];
+
+      await Apify.pushData({
+        origionalUrl,
+        normalizedUrl,
+        errorMessage,
+        "#isFailed": true
+      });
+    }
+  });
+
+  await basicCrawler.run();
+});
